7f15d8615f91d97d1c1a62d6ac433d12b8372faf
zefviktor <zefviktor@gmail.com>
2022-08-14 15:03:17 +0200

    add 4 button + slyle

--- src/components/SubscriptionBlock1.js
+++ src/components/SubscriptionBlock1.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import WatchIcon from '../res/image/watch.svg';
@@ -18,10 +19,57 @@ function SubscriptionBlock1() {
       </View>
       <View style={styles.priceBlock}>
         <View style={styles.priceButton}>
-          <Text>1 месяц</Text>
-          <View>
-            <Text>279 ₽</Text>
-            <Text>79 ₽</Text>
+          <View style={styles.priceButtonItem}>
+            <Text style={styles.priceButtonItemMonthText}>1 month</Text>
+          </View>
+          <View style={styles.priceButtonItem}>
+            <Text style={{display: 'none'}}>Популярно</Text>
+          </View>
+          <View style={styles.priceButtonItem}>
+            <Text style={styles.priceButtonItemOldText}>279 $</Text>
+            <Text style={styles.priceButtonItemNewText}>79 $</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.priceBlock}>
+        <View style={styles.priceButton}>
+          <View style={styles.priceButtonItem}>
+            <Text style={styles.priceButtonItemMonthText}>3 month</Text>
+          </View>
+          <View style={styles.priceButtonItem}>
+            <Text style={{display: 'none'}}>Популярно</Text>
+          </View>
+          <View style={styles.priceButtonItem}>
+            <Text style={styles.priceButtonItemOldText}>749 $</Text>
+            <Text style={styles.priceButtonItemNewText}>249 $</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.priceBlock}>
+        <View style={styles.priceButton}>
+          <View style={styles.priceButtonItem}>
+            <Text style={styles.priceButtonItemMonthText}>6 month</Text>
+          </View>
+          <View style={styles.priceButtonItem}>
+            <Text style={{display: 'none'}}>Популярно</Text>
+          </View>
+          <View style={styles.priceButtonItem}>
+            <Text style={styles.priceButtonItemOldText}>490 $</Text>
+            <Text style={styles.priceButtonItemNewText}>1290 $</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.priceBlock}>
+        <View style={styles.priceButton}>
+          <View style={styles.priceButtonItem}>
+            <Text style={styles.priceButtonItemMonthText}>1 year</Text>
+          </View>
+          <View style={styles.priceButtonItem}>
+            <Text style={{display: 'flex'}}> Popularly</Text>
+          </View>
+          <View style={styles.priceButtonItem}>
+            <Text style={styles.priceButtonItemOldText}>1145 $</Text>
+            <Text style={styles.priceButtonItemNewText}>2290 $</Text>
           </View>
         </View>
       </View>
@@ -72,9 +120,48 @@ const styles = StyleSheet.create({
     marginTop: 16,
   },
   priceButton: {
+    display: 'flex',
+    flexDirection: 'row',
     marginTop: 8,
+    padding: 16,
     backgroundColor: '#FFFFFF',
     borderRadius: 12,
+  },
+  priceButtonItem: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  priceButtonItemMonthText: {
+    // fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: 18,
+    lineHeight: 22,
+    display: 'flex',
+    alignItems: 'center',
+    color: '#15284E',
+  },
+  priceButtonItemOldText: {
+    // fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 14,
+    lineHeight: 18,
+    display: 'flex',
+    alignItems: 'center',
+    color: '#919AAF',
+  },
+  priceButtonItemNewText: {
+    // fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 14,
+    lineHeight: 18,
+    display: 'flex',
+    alignItems: 'center',
+    color: '#15284E',
   },
 });
 
